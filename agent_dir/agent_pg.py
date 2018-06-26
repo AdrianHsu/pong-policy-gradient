ee10a47ae925dae548c31136aec4a27a64b87045
AdrianHsu <adrianhsu1995@gmail.com>
2018-06-26 23:13:24 +0800

    first version

--- agent_dir/agent_pg.py
+++ agent_dir/agent_pg.py
@@ -18,9 +18,9 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 print(config)
 
-
+actions = {'up': 1, 'down': 0}
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'reward', 'next_state', 'done'))
+                        ('state', 'action', 'reward'))
 
 
 def prepro(o,image_size=[80,80]):
@@ -36,9 +36,8 @@ def prepro(o,image_size=[80,80]):
       Grayscale image, shape: (80, 80, 1)
   
   """
-
-  y = o.astype(np.uint8)
-
+  y = 0.2126 * o[:, :, 0] + 0.7152 * o[:, :, 1] + 0.0722 * o[:, :, 2]
+  y = y.astype(np.uint8)
   resized = scipy.misc.imresize(y, image_size)
   return np.expand_dims(resized.astype(np.float32),axis=2)
 
@@ -54,23 +53,32 @@ class Agent_PG(Agent):
     self.args = args
     self.batch_size = args.batch_size
     self.lr = args.learning_rate
+    self.gamma = args.gamma
+    self.hidden_dim = 1024
     self.action_dim = env.action_space.n # 6
     self.state_dim = env.observation_space.shape[0] # 210
     self.memory = []
     self.global_step = tf.Variable(0, trainable=False)
     self.add_global = self.global_step.assign_add(1)
     self.step = 0
-
+    self.s = tf.placeholder(tf.float32, [None, 6400], name='s')
+    self.act = tf.placeholder(tf.float32, [None, 1], name='act')
+    self.advantage =  tf.placeholder(
+            tf.float32, [None, 1], name='advantage')
+    
+    self.up_prob = self.build_net()
+    self.buildOptimizer()
 
     self.ckpts_path = self.args.save_dir + "pg.ckpt"
     self.saver = tf.train.Saver(max_to_keep = 3)
     self.sess = tf.Session(config=config)
+    self.writer = tf.summary.FileWriter(self.args.log_dir, graph=self.sess.graph)
 
     if args.test_pg:
       #you can load your model here
       print('loading trained model')
       ckpt = tf.train.get_checkpoint_state(self.args.save_dir)
-      print(ckpt)
+      print('ckpt', ckpt)
       if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
         print('Reloading model parameters..')
         self.saver.restore(self.sess, ckpt.model_checkpoint_path)
@@ -85,7 +93,7 @@ class Agent_PG(Agent):
 
   def init_model(self):
     ckpt = tf.train.get_checkpoint_state(self.args.save_dir)
-    print(ckpt)
+    print('ckpt: ', ckpt)
     if self.args.load_saver and ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
         print('Reloading model parameters..')
         self.saver.restore(self.sess, ckpt.model_checkpoint_path)
@@ -95,6 +103,43 @@ class Agent_PG(Agent):
     else:
         print('Created new model parameters..')
         self.sess.run(tf.global_variables_initializer())
+
+  def build_net(self):
+    with tf.variable_scope('fc1'):
+      W_fc1 = self.init_W(shape=[6400, self.hidden_dim])
+      b_fc1 = self.init_b(shape=[self.hidden_dim])
+      fc1 = tf.nn.bias_add(tf.matmul(self.s, W_fc1), b_fc1)
+      h_fc1 = tf.nn.relu(fc1)
+    with tf.variable_scope('fc2'):
+      W_fc2 = self.init_W(shape=[self.hidden_dim, 1])
+      b_fc2 = self.init_b(shape=[1])
+      fc2 = tf.nn.bias_add(tf.matmul(h_fc1, W_fc2), b_fc2)
+      up_prob = tf.nn.sigmoid(fc2)
+
+    return up_prob
+
+  def buildOptimizer(self):
+
+    self.loss = tf.losses.log_loss(labels=self.act, predictions=self.up_prob, weights=self.advantage)
+    self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+
+
+  def init_W(self, shape, name='weights', 
+    w_initializer=tf.truncated_normal_initializer(0, 1e-2)):
+
+    return tf.get_variable(
+      name=name,
+      shape=shape, 
+      initializer=w_initializer)
+
+  def init_b(self, shape, name='biases', 
+    b_initializer = tf.constant_initializer(1e-2)):
+
+    return tf.get_variable(
+      name=name,
+      shape=shape,
+      initializer=b_initializer)
+
 
   def init_game_setting(self):
     """
@@ -108,15 +153,45 @@ class Agent_PG(Agent):
     ##################
     pass
 
-  def storeTransition(self, s, action, reward, s_, done):
-    tr = Transition(s, action, reward, s_, done)
+  def storeTransition(self, s, action, reward):
+    tr = Transition(s, action, reward)
     self.memory.append(tr)
     self.step = self.sess.run(self.add_global)
     # print(len(self.memory))
 
   def learn(self):
-    pass
 
+    state_batch = [mem.state for mem in self.memory]
+    state_batch = np.array(state_batch).reshape(-1, 6400)
+    action_batch = [mem.action for mem in self.memory]
+    action_batch = np.array(action_batch).reshape(-1, 1)
+
+    reward_batch = self.discount_and_norm_rewards()
+    # reward_batch = [mem.reward for mem in self.memory]
+    reward_batch = np.array(reward_batch).reshape(-1, 1)
+
+    self.sess.run(self.train_op, feed_dict={
+        self.s: state_batch,
+        self.act: action_batch,
+        self.advantage: reward_batch
+      })
+    self.memory.clear()
+
+  def discount_and_norm_rewards(self):
+    reward_batch = [mem.reward for mem in self.memory]
+    discounted = np.zeros_like(reward_batch)
+    for t in range(len(reward_batch)):
+      discounted_sum = 0
+      discount = 1
+      for k in range(t, len(reward_batch)):
+        discounted_sum += reward_batch[k] * discount
+        discount *= self.gamma
+        if reward_batch[k] != 0:
+          break
+      discounted[t] = discounted_sum
+    discounted -= np.mean(discounted)
+    discounted /= np.std(discounted)
+    return discounted
 
   def train(self):
     """
@@ -125,21 +200,42 @@ class Agent_PG(Agent):
     pbar = tqdm(range(self.args.episode_start, self.args.num_episodes))
 
     # 1, 5 : up; 3, 4: down; 2, 6: stop
+    avg_reward = []
     for episode in pbar:
       obs = self.env.reset()
+      obs = prepro(obs)
       self.init_game_setting()
       episode_reward = 0.0
+
       for s in range(self.args.max_num_steps):
         action = self.make_action(obs, test=False)
-        obs_, reward, done, info = self.env.step(action)
+        obs_, reward, done, _ = self.env.step(action)
         episode_reward += reward
-        self.storeTransition(obs, action, reward, obs_, done)
-        obs = obs_
-        if done:          
+        self.storeTransition(obs, action, reward)
+
+        if self.step % self.args.saver_steps == 0 and episode != 0:
+          ckpt_path = self.saver.save(self.sess, self.ckpts_path, global_step = self.step)
+          print(color("\nStep: " + str(self.step) + ", Saver saved: " + ckpt_path, fg='white', bg='blue', style='bold'))
+
+        obs = prepro(obs_)
+        if done:   
+          episode_len = s       
           break
-      pbar.set_description("step: " + str(self.step) +  ", reward, " +  str(episode_reward))
-      vt = self.learn()
-      self.memory.clear()
+
+      # add summary for all episodes
+      summary = tf.Summary(value=[tf.Summary.Value(tag="rewards", simple_value=episode_reward)])
+      self.writer.add_summary(summary, episode)
+
+      avg_reward.append(episode_reward) 
+      if episode % self.args.batch_size == 0 and episode != 0:
+        avg_rew = np.mean(avg_reward)
+        avg_reward.clear()
+        memory_len = len(self.memory)
+        self.learn()
+        print(color("\n[Train] Avg Reward (30 rounds): " + "{:.2f}".format(avg_rew) + ", Memory Length: " + str(memory_len), fg='red', bg='white'))
+
+      pbar.set_description("step: " + str(self.step) +  ", reward, " +  str(episode_reward) + ", episode length: " + str(episode_len))
+      
 
 
 
@@ -158,5 +254,12 @@ class Agent_PG(Agent):
     ##################
     # YOUR CODE HERE #
     ##################
-    return self.env.get_random_action()
+    prob = self.sess.run(self.up_prob, feed_dict={self.s: observation.reshape((1, -1))})
+
+    if prob > 0.5:
+      return actions['up']
+    else:
+      return actions['down']
+
+    # return self.env.get_random_action()
 
