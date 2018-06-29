65475740c6d208b41e4f6b89d74924afe336bf34
AdrianHsu <adrianhsu1995@gmail.com>
2018-06-29 15:41:15 +0800

    16 batch

--- argument.py
+++ argument.py
@@ -7,7 +7,7 @@ def add_arguments(parser):
         parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
         parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for training')
     '''
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--learning_rate', type=float, default=0.0005)
     parser.add_argument('--num_episodes', type=int, default=30000)
     parser.add_argument('--episode_start', type=int, default=0)
